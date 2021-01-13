f2f1c87630fa90a6f72c3885e6e965a61f1d9f7e
pr1m0x <kraemer.timo1@gmail.com>
2021-01-13 10:13:18 +0100

    finished bonuses

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,570 +1,238 @@
-// // Iteration #1: Find the maximum
+// Iteration #1: Find the maximum
 
-// const maxOfTwoNumbers = (num1, num2) => {
-//   if (num1 < num2) return num2;
-//   return num1;
-// };
+const maxOfTwoNumbers = (num1, num2) => {
+  if (num1 < num2) return num2;
+  return num1;
+};
 
-// // Iteration #2: Find longest word
-// const words = [
-//   'mystery',
-//   'brother',
-//   'aviator',
-//   'crocodile',
-//   'pearl',
-//   'orchard',
-//   'crackpot',
-// ];
+// Iteration #2: Find longest word
+const words = [
+  'mystery',
+  'brother',
+  'aviator',
+  'crocodile',
+  'pearl',
+  'orchard',
+  'crackpot',
+];
 
-// const findLongestWord = (wordsArr) => {
-//   if (wordsArr.length === 0) return null;
+const findLongestWord = (wordsArr) => {
+  if (wordsArr.length === 0) return null;
 
-//   let longestWord = '';
-//   for (let i = 0; i < wordsArr.length; i++) {
-//     wordsArr[i].length > longestWord.length
-//       ? (longestWord = wordsArr[i])
-//       : null;
-//   }
+  let longestWord = '';
+  for (let i = 0; i < wordsArr.length; i++) {
+    wordsArr[i].length > longestWord.length
+      ? (longestWord = wordsArr[i])
+      : null;
+  }
 
-//   return longestWord;
-// };
+  return longestWord;
+};
 
-// findLongestWord(words);
+findLongestWord(words);
 
-// // Iteration #3: Calculate the sum
-// const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
-// const numbers2 = ['6', '12', 2];
+// Iteration #3: Calculate the sum
+const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
+const numbers2 = ['6', '12', 2];
 
-// const sumNumbers = (numbersArr) => {
-//   let sum = 0;
+const sumNumbers = (numbersArr) => {
+  let sum = 0;
 
-//   numbersArr.forEach((number) => {
-//     sum += number;
-//   });
+  numbersArr.forEach((number) => {
+    sum += number;
+  });
 
-//   return sum;
-// };
+  return sum;
+};
 
-// sumNumbers(numbers);
+sumNumbers(numbers);
 
-// const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10, []];
+const mixedArr = [6, 12, 'miami', 1, true, 'barca', '200', 'lisboa', 8, 10, []];
 
-// const sum = (mixedArr) => {
-//   if (mixedArr.length === 0) return 0;
-//   let showError = false;
+const sum = (mixedArr) => {
+  if (mixedArr.length === 0) return 0;
+  let showError = false;
 
-//   mixedArr.forEach((el) => {
-//     if (typeof el === 'object') showError = true;
-//   });
+  mixedArr.forEach((el) => {
+    if (typeof el === 'object') showError = true;
+  });
 
-//   // if (showError) throw new Error("Unsupported data type sir or ma'am");
+  // if (showError) throw new Error("Unsupported data type sir or ma'am");
 
-//   let sum = 0;
+  let sum = 0;
 
-//   mixedArr.forEach((el) => {
-//     switch (typeof el) {
-//       case 'boolean':
-//         if (el === true) sum += 1;
-//         break;
-//       case 'string':
-//         sum += el.length;
-//         break;
-//       case 'number':
-//         sum += el;
-//         break;
-//     }
-//   });
+  mixedArr.forEach((el) => {
+    switch (typeof el) {
+      case 'boolean':
+        if (el === true) sum += 1;
+        break;
+      case 'string':
+        sum += el.length;
+        break;
+      case 'number':
+        sum += el;
+        break;
+    }
+  });
 
-//   return sum;
-// };
+  return sum;
+};
 
-// sum(mixedArr);
+sum(mixedArr);
 
-// // Iteration #4: Calculate the average
-// // Level 1: Array of numbers
-// const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
+// Iteration #4: Calculate the average
+// Level 1: Array of numbers
+const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-// const averageNumbers = (numbersArr) => {
-//   if (numbersArr.length === 0) return null;
+const averageNumbers = (numbersArr) => {
+  if (numbersArr.length === 0) return null;
 
-//   const reducer = (accumulator, currentValue) => accumulator + currentValue;
-//   const sum = numbersArr.reduce(reducer);
-//   return sum / numbersArr.length;
-// };
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+  const sum = numbersArr.reduce(reducer);
+  return sum / numbersArr.length;
+};
 
-// averageNumbers(numbersAvg);
+averageNumbers(numbersAvg);
 
-// // Level 2: Array of strings
-// const wordsArrTest = ['seat', 'correspond'];
+// Level 2: Array of strings
+const wordsArrTest = ['seat', 'correspond'];
 
-// const wordsArr = [
-//   'seat',
-//   'correspond',
-//   'linen',
-//   'motif',
-//   'hole',
-//   'smell',
-//   'smart',
-//   'chaos',
-//   'fuel',
-//   'palace',
-// ];
+const wordsArr = [
+  'seat',
+  'correspond',
+  'linen',
+  'motif',
+  'hole',
+  'smell',
+  'smart',
+  'chaos',
+  'fuel',
+  'palace',
+];
 
-// const averageWordLength = (wordsArrTest) => {
-//   if (wordsArrTest.length === 0) return null;
+const averageWordLength = (wordsArrTest) => {
+  if (wordsArrTest.length === 0) return null;
 
-//   const str = wordsArrTest.join('');
-//   return str.length / wordsArrTest.length;
-// };
+  const str = wordsArrTest.join('');
+  return str.length / wordsArrTest.length;
+};
 
-// averageWordLength(wordsArr);
+averageWordLength(wordsArr);
 
-// const otherMixedArr = [
-//   6,
-//   12,
-//   'miami',
-//   1,
-//   true,
-//   'barca',
-//   '200',
-//   'lisboa',
-//   8,
-//   10,
-// ];
+const otherMixedArr = [
+  6,
+  12,
+  'miami',
+  1,
+  true,
+  'barca',
+  '200',
+  'lisboa',
+  8,
+  10,
+];
 
-// const avg = (arr) => {
-//   if (arr.length === 0) return null;
+const avg = (arr) => {
+  if (arr.length === 0) return null;
 
-//   let sum = 0;
+  let sum = 0;
 
-//   arr.forEach((el) => {
-//     switch (typeof el) {
-//       case 'boolean':
-//         el ? (sum += 1) : (sum += 0);
-//         break;
-//       case 'string':
-//         sum += el.length;
-//         break;
-//       case 'number':
-//         sum += el;
-//         break;
-//     }
-//   });
+  arr.forEach((el) => {
+    switch (typeof el) {
+      case 'boolean':
+        el ? (sum += 1) : (sum += 0);
+        break;
+      case 'string':
+        sum += el.length;
+        break;
+      case 'number':
+        sum += el;
+        break;
+    }
+  });
 
-//   const avgSum = sum / arr.length;
-//   return parseFloat(avgSum.toFixed(2));
-// };
+  const avgSum = sum / arr.length;
+  return parseFloat(avgSum.toFixed(2));
+};
 
-// avg(otherMixedArr);
+avg(otherMixedArr);
 
-// // Iteration #5: Unique arrays
-// const wordsUnique = [
-//   'crab',
-//   'poison',
-//   'contagious',
-//   'simple',
-//   'bring',
-//   'sharp',
-//   'playground',
-//   'poison',
-//   'communion',
-//   'simple',
-//   'bring',
-// ];
+// Iteration #5: Unique arrays
+const wordsUnique = [
+  'crab',
+  'poison',
+  'contagious',
+  'simple',
+  'bring',
+  'sharp',
+  'playground',
+  'poison',
+  'communion',
+  'simple',
+  'bring',
+];
 
-// const uniquifyArray = (wordsArr) => {
-//   if (wordsArr.length === 0) return null;
+const uniquifyArray = (wordsArr) => {
+  if (wordsArr.length === 0) return null;
 
-//   let excludeFromArr = [];
+  let excludeFromArr = [];
 
-//   wordsArr.forEach((el) => {
-//     if (!excludeFromArr.includes(el)) excludeFromArr.push(el);
-//   });
+  wordsArr.forEach((el) => {
+    if (!excludeFromArr.includes(el)) excludeFromArr.push(el);
+  });
 
-//   return excludeFromArr;
-// };
+  return excludeFromArr;
+};
 
-// uniquifyArray(wordsUnique);
+uniquifyArray(wordsUnique);
 
-// // Iteration #6: Find elements
-// const wordsFind = [
-//   'machine',
-//   'subset',
-//   'trouble',
-//   'starting',
-//   'matter',
-//   'eating',
-//   'truth',
-//   'disobedience',
-// ];
+// Iteration #6: Find elements
+const wordsFind = [
+  'machine',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'truth',
+  'disobedience',
+];
 
-// const doesWordExist = (wordsArr, word) => {
-//   if (wordsArr.length === 0) return null;
-//   return wordsArr.includes(word) ? true : false;
-// };
+const doesWordExist = (wordsArr, word) => {
+  if (wordsArr.length === 0) return null;
+  return wordsArr.includes(word) ? true : false;
+};
 
-// doesWordExist(wordsFind, 'haus');
+doesWordExist(wordsFind, 'haus');
 
-// // Iteration #7: Count repetition
-// const wordsCount = [
-//   'machine',
-//   'matter',
-//   'subset',
-//   'trouble',
-//   'starting',
-//   'matter',
-//   'eating',
-//   'matter',
-//   'truth',
-//   'disobedience',
-//   'matter',
-// ];
+// Iteration #7: Count repetition
+const wordsCount = [
+  'machine',
+  'matter',
+  'subset',
+  'trouble',
+  'starting',
+  'matter',
+  'eating',
+  'matter',
+  'truth',
+  'disobedience',
+  'matter',
+];
 
-// const howManyTimes = (wordsCount, word) => {
-//   let counter = 0;
+const howManyTimes = (wordsCount, word) => {
+  let counter = 0;
 
-//   for (let i = 0; i < wordsCount.length; i++) {
-//     if (wordsCount[i] === word) counter++;
-//   }
+  for (let i = 0; i < wordsCount.length; i++) {
+    if (wordsCount[i] === word) counter++;
+  }
 
-//   return counter;
-// };
+  return counter;
+};
 
-// howManyTimes(wordsCount);
+howManyTimes(wordsCount);
 
 // Iteration #8: Bonus
-
-const matrix = [
-  [8, 2, 22, 97, 38, 15, 0, 40, 0, 75, 4, 5, 7, 78, 52, 12, 50, 77, 91, 8],
-  [
-    49,
-    49,
-    99,
-    40,
-    17,
-    81,
-    18,
-    57,
-    60,
-    87,
-    17,
-    40,
-    98,
-    43,
-    69,
-    48,
-    4,
-    56,
-    62,
-    0,
-  ],
-  [
-    81,
-    49,
-    31,
-    73,
-    55,
-    79,
-    14,
-    29,
-    93,
-    71,
-    40,
-    67,
-    53,
-    88,
-    30,
-    3,
-    49,
-    13,
-    36,
-    65,
-  ],
-  [52, 70, 95, 23, 4, 60, 11, 42, 69, 24, 68, 56, 1, 32, 56, 71, 37, 2, 36, 91],
-  [
-    22,
-    31,
-    16,
-    71,
-    51,
-    67,
-    63,
-    89,
-    41,
-    92,
-    36,
-    54,
-    22,
-    40,
-    40,
-    28,
-    66,
-    33,
-    13,
-    80,
-  ],
-  [
-    24,
-    47,
-    32,
-    60,
-    99,
-    3,
-    45,
-    2,
-    44,
-    75,
-    33,
-    53,
-    78,
-    36,
-    84,
-    20,
-    35,
-    17,
-    12,
-    50,
-  ],
-  [
-    32,
-    98,
-    81,
-    28,
-    64,
-    23,
-    67,
-    10,
-    26,
-    38,
-    40,
-    67,
-    59,
-    54,
-    70,
-    66,
-    18,
-    38,
-    64,
-    70,
-  ],
-  [
-    67,
-    26,
-    20,
-    68,
-    2,
-    62,
-    12,
-    20,
-    95,
-    63,
-    94,
-    39,
-    63,
-    8,
-    40,
-    91,
-    66,
-    49,
-    94,
-    21,
-  ],
-  [
-    24,
-    55,
-    58,
-    5,
-    66,
-    73,
-    99,
-    26,
-    97,
-    17,
-    78,
-    78,
-    96,
-    83,
-    14,
-    88,
-    34,
-    89,
-    63,
-    72,
-  ],
-  [21, 36, 23, 9, 75, 0, 76, 44, 20, 45, 35, 14, 0, 61, 33, 97, 34, 31, 33, 95],
-  [78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 3, 80, 4, 62, 16, 14, 9, 53, 56, 92],
-  [
-    16,
-    39,
-    5,
-    42,
-    96,
-    35,
-    31,
-    47,
-    55,
-    58,
-    88,
-    24,
-    0,
-    17,
-    54,
-    24,
-    36,
-    29,
-    85,
-    57,
-  ],
-  [86, 56, 0, 48, 35, 71, 89, 7, 5, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58],
-  [
-    19,
-    80,
-    81,
-    68,
-    5,
-    94,
-    47,
-    69,
-    28,
-    73,
-    92,
-    13,
-    86,
-    52,
-    17,
-    77,
-    4,
-    89,
-    55,
-    40,
-  ],
-  [4, 52, 8, 83, 97, 35, 99, 16, 7, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66],
-  [
-    88,
-    36,
-    68,
-    87,
-    57,
-    62,
-    20,
-    72,
-    3,
-    46,
-    33,
-    67,
-    46,
-    55,
-    12,
-    32,
-    63,
-    93,
-    53,
-    69,
-  ],
-  [
-    4,
-    42,
-    16,
-    73,
-    38,
-    25,
-    39,
-    11,
-    24,
-    94,
-    72,
-    18,
-    8,
-    46,
-    29,
-    32,
-    40,
-    62,
-    76,
-    36,
-  ],
-  [
-    20,
-    69,
-    36,
-    41,
-    72,
-    30,
-    23,
-    88,
-    34,
-    62,
-    99,
-    69,
-    82,
-    67,
-    59,
-    85,
-    74,
-    4,
-    36,
-    16,
-  ],
-  [
-    20,
-    73,
-    35,
-    29,
-    78,
-    31,
-    90,
-    1,
-    74,
-    31,
-    49,
-    71,
-    48,
-    86,
-    81,
-    16,
-    23,
-    57,
-    5,
-    54,
-  ],
-  [
-    1,
-    70,
-    54,
-    71,
-    83,
-    51,
-    54,
-    69,
-    16,
-    92,
-    33,
-    48,
-    61,
-    43,
-    52,
-    1,
-    89,
-    19,
-    67,
-    48,
-  ],
-];
-
-const sampleMatrix = [
-  [1, 2, 3, 4, 5],
-  [1, 20, 3, 4, 5],
-  [1, 20, 3, 4, 5],
-  [1, 20, 3, 4, 5],
-  [1, 4, 3, 4, 5],
-];
-
-//! 1 Read first 4 elements
-//! 2 Create Product (*) and save it in greatestSum
-//! 3 Delete first Element of the first 4 elements, (pop()) add the next element of the iteration
-//! 4 Create other sum with this new 4 numbers
-//! 5 Compare first Sum with second Sum
-//! 6 if second Sum is greater than overwrite the greatestSum
 
 const createProduct = (arr) => {
   return arr.reduce(function (a, b) {
@@ -572,7 +240,7 @@ const createProduct = (arr) => {
   });
 };
 
-const greatestProduct = (matrix) => {
+const greatestProductVertically = (matrix) => {
   let greatestProduct = { matrix: [], product: 0 };
 
   for (let i = 0; i < matrix.length; i++) {
@@ -587,7 +255,6 @@ const greatestProduct = (matrix) => {
       // Setup first Product
       if (k === 4) {
         currentIteration.product = createProduct(currentIteration.matrix);
-        // console.log(currentIteration);
       }
 
       // After first Setup
@@ -612,40 +279,56 @@ const greatestProduct = (matrix) => {
       }
     }
   }
-  setTimeout(() => {
-    console.log(greatestProduct);
-  }, 2000);
+  return greatestProduct;
 };
 
-// const greatestProduct = (matrix) => {
-//   const greatestProduct = { sum: 0, matrix: [] };
+const greatestProductHorizontally = (matrix) => {
+  let greatestProduct = { matrix: [], product: 0 };
 
-//   for (let i = 0; i < matrix.length; i++) {
-//     const firstIteration = { sum: 1, matrix: [] };
-//     // const followingIterations = { sum: 1, matrix: [] };
-//     for (let k = 0; k < matrix.length; k++) {
-//       if (k < 4) {
-//         firstIteration.matrix.push(matrix[k][i]);
-//         if (k === 3) {
-//           const newProduct = createProduct(firstIteration.matrix);
-//           if (firstIteration.sum < newProduct) {
-//             greatestProduct.sum = newProduct;
-//           }
-//         }
-//       } else {
-//         const firstIterationC = { ...firstIteration };
-//         firstIterationC.matrix.pop();
-//         firstIterationC.matrix.push(matrix[k][i]);
+  for (let i = 0; i < matrix.length; i++) {
+    let currentIteration = { matrix: [], product: 0 };
 
-//         var result = firstIterationC.matrix.reduce(function (a, b) {
-//           return a * b;
-//         });
+    for (let k = 0; k < matrix.length; k++) {
+      // Setup first Matrix
+      if (k < 4 && currentIteration.product === 0) {
+        currentIteration.matrix.push(matrix[i][k]);
+      }
 
-//         // console.log(result);
-//       }
-//     }
-//     console.log(firstIteration);
-//   }
-// };
+      // Setup first Product
+      if (k === 4) {
+        currentIteration.product = createProduct(currentIteration.matrix);
+      }
 
-greatestProduct(sampleMatrix);
+      // Run after first Product creation
+      if (currentIteration.product > 0) {
+        // Copy currentIteration Object for manipulation
+        let currentIterationC = { ...currentIteration };
+
+        // Delete first Element of Array
+        currentIterationC.matrix.shift();
+        currentIterationC.matrix.push(matrix[k][i]);
+
+        const newProduct = createProduct(currentIterationC.matrix);
+        currentIteration.product = newProduct;
+
+        if (currentIteration.product < currentIterationC.product) {
+          currentIteration = currentIterationC;
+        }
+
+        if (greatestProduct.product < currentIteration.product) {
+          greatestProduct = currentIteration;
+        }
+      }
+    }
+  }
+  return greatestProduct;
+};
+
+const greatestProduct = (matrix) => {
+  const result = greatestProductVertically(matrix);
+  const result2 = greatestProductHorizontally(matrix);
+
+  return result.product < result2.product ? result2.product : result.product;
+};
+
+greatestProduct(matrixOne);
